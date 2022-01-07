330e45c5082b95c2a0bd8438c2605e6bc53df05e
dclechok <dclechok@gmail.com>
2022-01-07 10:59:37 -0500

    added title bar, styled background and font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TitleBar from './TitleBar';
 import GameBoard from './GameBoard';
 
 function App() { //gameboard
   return (
     <div className="App">
+        <TitleBar />
         <GameBoard />
     </div>
   );
---
+++ src/TitleBar.js
@@ -0,0 +1,10 @@
+function TitleBar() {
+  return (
+    <div>
+      <h1>Welcome to JSnake</h1>
+      <h2>Written by Daniel C. Lechok</h2>
+    </div>
+  );
+}
+
+export default TitleBar;

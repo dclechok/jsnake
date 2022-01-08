bce97ef6036a4914e85cb833ddabdd5a6d7c748e
dclechok <dclechok@gmail.com>
2022-01-07 20:02:33 -0500

    adding movement for snake

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,42 +1,83 @@
-import './GameBoard.css';
-import SnakeBuilder from './SnakeBuilder';
-import { useEffect, useState } from 'react';
+import "./GameBoard.css";
+import SnakeBuilder from "./SnakeBuilder";
+import { useEffect, useState } from "react";
 
-function GameBoard(){
+function GameBoard() {
+  const [direction, setDirection] = useState(null); //randomize starting direction
+  const [currentPosition, setCurrentPosition] = useState({
+    top: null,
+    left: null,
+  }); //update current position state
 
-    const [direction, setDirection] = useState('east'); //randomize starting direction
+  useEffect(() => {
+    let snakeHeadPos = document.getElementById("snake-head");
 
-    window.addEventListener('keyup', (e) => {
-        if(e.key === 'w') setDirection('north');
-        else if(e.key === 'a') setDirection('west');
-        else if(e.key === 's') setDirection('south');
-        else if(e.key === 'd') setDirection('east');
-    });
+    console.log(
+      snakeHeadPos.offsetTop,
+      snakeHeadPos.offsetWidth,
+      snakeHeadPos.offsetHeight,
+      snakeHeadPos.offsetLeft
+    );
 
-    useEffect(()=> {
-    let snakeHeadPos = document.getElementById('snake-head');
-    console.log(snakeHeadPos.offsetTop, snakeHeadPos.offsetWidth, snakeHeadPos.offsetHeight, snakeHeadPos.offsetLeft);
-    
-    async function moveSnake(){
-        if(direction === 'north') //top -= 1
-        if(direction === 'south') //top += 1
-        if(direction === 'west') // left -=1
-        if(direction === 'east') // right += 1
+    async function moveSnake() {
+      snakeHeadPos.style.position = "relative"; //relative to our game board]
+      window.addEventListener("keyup", (e) => {
+        if (e.key === "w") setDirection("north");
+        else if (e.key === "a") setDirection("west");
+        else if (e.key === "s") setDirection("south");
+        else if (e.key === "d") setDirection("east");
+      });
+
+        if (direction === "north") {
+          console.log("going north");
+          setTimeout(() => {
+            snakeHeadPos.style.top = `${currentPosition.top - 1}px`; // 380 is MAX *from* top
+          }, 50);
+          setCurrentPosition({
+            top: (currentPosition.top -= 1),
+            left: currentPosition.left,
+          });
+        } //top -= 1
+        if (direction === "south") {
+          console.log("going south");
+          setTimeout(() => {
+            snakeHeadPos.style.top = `${currentPosition.top + 1}px`; // 380 is MAX *from* top
+          }, 50);
+          setCurrentPosition({
+            top: (currentPosition.top += 1),
+            left: currentPosition.left,
+          });
+        } //top += 1
+        if (direction === "west") {
+          console.log("going west");
+          setTimeout(() => {
+            snakeHeadPos.style.left = `${currentPosition.left - 1}px`;
+          }, 50);
+          setCurrentPosition({
+            top: (currentPosition.top),
+            left: currentPosition.left -= 1,
+          });
+        } // left -=1
+      if (direction === "east") {
+        console.log("going east");
         setTimeout(() => {
-            snakeHeadPos.style.position = "relative"; //relative to our game board
-            snakeHeadPos.style.left = "380px"; // 380 is MAX
-            snakeHeadPos.style.top = "380px"; // 380 is MAX
-        }, 200);
+          snakeHeadPos.style.left = `${currentPosition.left + 1}px`;
+        }, 50);
+        setCurrentPosition({
+          top: currentPosition.top,
+          left: (currentPosition.left += 1),
+        });
+      }// right += 1
     }
     moveSnake();
-    }, []);
+    // setDirection(null);
+  }, [direction]);
 
-    return (
-        <div className="gameboard">
-            <SnakeBuilder /> 
-        </div>
-    )
-
+  return (
+    <div className="gameboard">
+      <SnakeBuilder />
+    </div>
+  );
 }
 
 export default GameBoard;

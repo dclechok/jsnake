36a22eb05ab6791a209dd9e8ccae9bfb7ade334f
dclechok <dclechok@gmail.com>
2022-01-08 10:46:01 -0500

    added smoother movement with directional keys, now have to implement a loop to continue movement

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 function GameBoard() {
   const [direction, setDirection] = useState(null); //randomize starting direction
+  const [moving, setMoving] = useState(true);
   const [currentPosition, setCurrentPosition] = useState({
     top: null,
     left: null,
@@ -20,60 +21,54 @@ function GameBoard() {
     );
 
     async function moveSnake() {
-
       window.addEventListener("keydown", (e) => {
         // our controls
-        if (e.key === "w") setDirection("north");
-        else if (e.key === "a") setDirection("west");
-        else if (e.key === "s") setDirection("south");
-        else if (e.key === "d") setDirection("east");
+        if (e.key === "w") {
+          console.log("going north");
+          setTimeout(() => {
+            snakeHeadPos.style.top = `${currentPosition.top - 1}px`; // 380 is MAX *from* top
+          }, 50);
+          setCurrentPosition({
+            top: (currentPosition.top -= 1),
+            left: currentPosition.left,
+          });
+          if (snakeHeadPos.offsetTop <= 153) console.log("dead - north wall"); //ran into a wall
+        }
+        if (e.key === "a") {
+          console.log("going west");
+          setTimeout(() => {
+            snakeHeadPos.style.left = `${currentPosition.left - 1}px`;
+          }, 50);
+          setCurrentPosition({
+            top: currentPosition.top,
+            left: (currentPosition.left -= 1),
+          });
+          if (snakeHeadPos.offsetLeft <= 342) console.log("dead - west wall"); //ran into a wall
+        } // left -=1}
+        if (e.key === "s") {
+          console.log("going south");
+          setTimeout(() => {
+            snakeHeadPos.style.top = `${currentPosition.top + 1}px`; // 380 is MAX *from* top
+          }, 50);
+          setCurrentPosition({
+            top: (currentPosition.top += 1),
+            left: currentPosition.left,
+          });
+          setDirection(null);
+          if (snakeHeadPos.offsetTop >= 533) console.log("dead - south wall");
+        }
+        if (e.key === "d") {
+          console.log("going east");
+          setTimeout(() => {
+            snakeHeadPos.style.left = `${currentPosition.left + 1}px`;
+          }, 50);
+          setCurrentPosition({
+            top: currentPosition.top,
+            left: (currentPosition.left += 1),
+          });
+          if (currentPosition.left >= 381) console.log("dead - east wall"); //ran into a wall
+        } // right += 1
       });
-
-      if (direction === "north") {
-        console.log("going north");
-        setTimeout(() => {
-          snakeHeadPos.style.top = `${currentPosition.top - 1}px`; // 380 is MAX *from* top
-        }, 50);
-        setCurrentPosition({
-          top: (currentPosition.top -= 1),
-          left: currentPosition.left,
-        });
-        if (snakeHeadPos.offsetTop <= 153) console.log("dead - north wall"); //ran into a wall
-      } //top -= 1
-      if (direction === "south") {
-        console.log("going south");
-        setTimeout(() => {
-          snakeHeadPos.style.top = `${currentPosition.top + 1}px`; // 380 is MAX *from* top
-        }, 50);
-        setCurrentPosition({
-          top: (currentPosition.top += 1),
-          left: currentPosition.left,
-        });
-        if (snakeHeadPos.offsetTop >= 533) console.log("dead - south wall"); //ran into a wall
-      } //top += 1
-      if (direction === "west") {
-        console.log("going west");
-        setTimeout(() => {
-          snakeHeadPos.style.left = `${currentPosition.left - 1}px`;
-        }, 50);
-        setCurrentPosition({
-          top: currentPosition.top,
-          left: (currentPosition.left -= 1),
-        });
-        if (snakeHeadPos.offsetLeft <= 342) console.log("dead - west wall"); //ran into a wall
-      } // left -=1
-
-      if (direction === "east") {
-        console.log("going east");
-        setTimeout(() => {
-          snakeHeadPos.style.left = `${currentPosition.left + 1}px`;
-        }, 50);
-        setCurrentPosition({
-          top: currentPosition.top,
-          left: (currentPosition.left += 1),
-        });
-        if (currentPosition.left >= 381) console.log("dead - east wall"); //ran into a wall
-      } // right += 1
     }
     moveSnake();
     // setDirection(null);
